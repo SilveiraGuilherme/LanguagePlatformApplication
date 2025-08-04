8f95c954cefdeacfe9ef7a2dec348145add145eb
Guilherme Silveira <108364424+SilveiraGuilherme@users.noreply.github.com>
2025-08-04 18:23:05 +0100

    Add CORS configuration for handling cross-origin requests

---
+++ BACKEND/src/main/java/com/project/languageplatform/security/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.project.languageplatform.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500", "https://purple-forest-07e4c2103.1.azurestaticapps.net"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
